3986dec5559c67627de1578c74e02ce96a0c3f5d
Fidelis E. Peter <fidelisepeter#gmail.com>
2023-08-31 23:14:45 -0700

    first comit

--- js/main.js
+++ js/main.js
@@ -512,33 +512,6 @@ var swiper = new Swiper(".ai__slider", {
       },
   });
 
-// Dark to light mode js
-const lightToDarkButton = document.getElementById("light--to-dark-button");
-lightToDarkButton?.addEventListener("click", function () {
-    if (localStorage.getItem("theme-color")) {
-      if (localStorage.getItem("theme-color") === "light") {
-        document.documentElement.classList.add("is_dark");
-        localStorage.setItem("theme-color", "dark");
-        lightToDarkButton?.classList.add("dark--mode");
-      } else {
-        document.documentElement.classList.remove("is_dark");
-        localStorage.setItem("theme-color", "light");
-        lightToDarkButton?.classList?.remove("dark--mode");
-      }
-    } else {
-      if (document.documentElement.classList.contains("is_dark")) {
-        document.documentElement.classList.remove("is_dark");
-        lightToDarkButton?.classList?.remove("dark--mode");
-        localStorage.setItem("theme-color", "light");
-      } else {
-        document.documentElement.classList.add("is_dark");
-        localStorage.setItem("theme-color", "dark");
-        lightToDarkButton?.classList.add("dark--mode");
-      }
-    }
-});
-
-
 
   //Modal Gallery
   var swiper = new Swiper(".details__gallery", {
